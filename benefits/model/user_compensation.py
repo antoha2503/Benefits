9a1274e5f80281624d811ef53e5dafdc40e9cf74
home <anton.tytenko@gmail.com>
2026-02-18 22:32:27 +0200

    directory structure changes

---
+++ benefits/model/user_compensation.py
@@ -0,0 +1,59 @@
+from odoo import fields, models, api
+from odoo.exceptions import UserError
+
+class UserCompensation(models.Model):
+    _name = 'user.compensation'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _rec_name = 'employee_id'
+
+    employee_id = fields.Many2one('hr.employee')
+    currency_id = fields.Many2one('res.currency', default=lambda self: self.env.company.currency_id)
+    amount = fields.Monetary(currency_field='currency_id', readonly=False)
+    state = fields.Selection([
+        ('waiting', 'Waiting'),
+        ('done', 'Dono'),
+        ('canceled', 'Canceled')
+    ], default='waiting')
+    dashboard_id = fields.Many2one('benefits.dashboard', ondelete='cascade')
+    type_compensation = fields.Many2one('category.compensation', string='Compensation Type', required=True)
+
+    # import_file = fields.Binary('Import file')
+
+    date_create = fields.Date(string='Date', default=fields.Date.context_today, readonly=True)
+
+    # Поле для отображения прикрепленных файлов
+    attachment_ids = fields.Many2many(
+        'ir.attachment',
+        relation='user_compensation_attachment_rel',
+        column1='compensation_id',
+        column2='attachment_id',
+        compute='_compute_attachment_ids',
+        string="Attachments"
+    )
+
+    def _compute_attachment_ids(self):
+        for record in self:
+            record.attachment_ids = self.env['ir.attachment'].search([
+                ('res_model', '=', self._name),
+                ('res_id', '=', record.id)
+            ])
+
+    def action_canceled(self):
+        for record in self:
+            if record.state == 'done':
+                raise UserError("Canceled compensation cannot be marked as canceled.")
+        self.dashboard_id.write({'total':self.dashboard_id.total+self.amount})
+        self.write({'state': 'canceled'})
+
+    def action_done(self):
+        for record in self:
+            if record.state == 'canceled':
+                raise UserError("Canceled compensation cannot be marked as done.")
+        self.write({'state': 'done'})
+
+    # @api.model
+    # def _add_benefits_employee(self):
+    #     dashboards = self.env['benefits.dashboard'].search([])
+    #
+    #     for dashboard in dashboards:
+    #         dashboard.total += 1000
