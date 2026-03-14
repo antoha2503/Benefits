2f96b199d76b680b96c0d2a5885f26c9a90dcabe
antoha2503 <114880081+antoha2503@users.noreply.github.com>
2026-03-14 14:38:33 +0200

    Update __manifest__.py

--- benefits/__manifest__.py
+++ benefits/__manifest__.py
@@ -10,7 +10,7 @@
     "license": "LGPL-3",
     'price': "0",
     'currency': 'USD',
-    'images': ['static/description/icon.png'],
+    'images': ['static/description/icon_screenshot.png'],
     "depends": [
         "base",
         "purchase"
