c9493d90d018d1ca8c524e49926e4efa824f2682
antoha2503 <114880081+antoha2503@users.noreply.github.com>
2026-04-02 23:45:38 +0300

    Update __manifest__.py

--- benefits/__manifest__.py
+++ benefits/__manifest__.py
@@ -4,7 +4,7 @@
     "category": "Human Resources",
     "summary": "The Benefits Dashboard module allows companies to automatically allocate funds for employee benefits and submit reimbursement requests for expenses such as business trips, health, education, and other corporate perks. Everything is managed directly in Odoo, easily and transparently.",
     "description": "The Benefits Dashboard module allows companies to automatically allocate funds for employee benefits and submit reimbursement requests for expenses such as business trips, health, education, and other corporate perks. Everything is managed directly in Odoo, easily and transparently.",
-    "author": "Anton Tytenko",
+    "author": "TytencoSoft",
     "website": "https://github.com/antoha2503",
     "license": "LGPL-3",
     'price': "0",
